7893b2769f9dd21559534fab501f28a0cf8bdf8d
Oleksandr Riadinskyi <oleksandr.riadinskyi@bilsoft.dev>
2021-11-10 13:59:03 +0200

    Initial

---
+++ HL7Reproduce/Program.cs
@@ -0,0 +1,19 @@
+﻿using HL7.Dotnetcore;
+
+namespace HL7Reproduce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var message = new Message();
+
+            var encoding = new HL7Encoding();
+
+            var msg = new Segment("MSG", encoding);
+            message.AddNewSegment(msg);
+
+            message.SerializeMessage(true);
+        }
+    }
+}
